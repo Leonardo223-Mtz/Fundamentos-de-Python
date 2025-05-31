b5f95834b2646da6e6d5495a6cc959702cd04072
Leonardo223-Mtz <leonardo.martinez223@gmail.com>
2025-05-31 12:52:37 -0600

    Update Fundamentos.py

--- Fundamentos.py
+++ Fundamentos.py
@@ -1,16 +1,26 @@
+#Archivo para operadores aritmeticos
+
 def Suma(a,b):
     return a+b
 
 def Resta(a,b):
     return a - b
 
-print("---------- Operadores aritmeticos --------------")
-print("Operador suma, 5 + 6 = ",Suma(5,6))
-print("Operador resta, 5 - 6 = ",Resta(5,6))
-print("Operador Multiplicacion, 5 * 6 = ",5*6)
-print("Operador Division, 5 / 6 = ",5/6)
-print("Operador Division entera, 5 // 6 = ",5//6)
-print("Operador Módulo, 5 % 6 = ",5%6)
-print("Operador Exponente, 5 ** 6 = ",5**6)
-print("Operador +, +5 = ",+5)
-print("Operador -, -5 = ",-5)
+if __name__ == "__main__":
+
+    print("---------- Operadores aritmeticos --------------")
+    print("Operador suma, 5 + 6 = ",Suma(5,6))
+    print("Operador resta, 5 - 6 = ",Resta(5,6))
+    print("Operador Multiplicacion, 5 * 6 = ",5*6)
+    print("Operador Division, 5 / 6 = ",5/6)
+    print("Operador Division entera, 5 // 6 = ",5//6)
+    print("Operador Módulo, 5 % 6 = ",5%6)
+    print("Operador Exponente, 5 ** 6 = ",5**6)
+    print("Operador +, +5 = ",+5)
+    print("Operador -, -5 = ",-5)
+
+    ''' Comentario de varias lineas
+    Línea 2 
+    Etc....'''
+
+
