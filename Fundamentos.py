05cf9704b6648ff1beec9d74dd74739bf4ea37fb
Leonardo223-Mtz <leonardo.martinez223@gmail.com>
2025-05-31 12:44:21 -0600

    Create Fundamentos.py

---
+++ Fundamentos.py
@@ -0,0 +1,17 @@
+def Suma(a,b):
+    return a+b
+
+def Resta(a,b):
+    return a - b
+
+print("---------- Operadores aritmeticos --------------")
+print("Operador suma, 5 + 6 = ",Suma(5,6))
+print("Operador resta, 5 - 6 = ",Resta(5,6))
+print("Operador resta, 5 - 6 = ",Resta(5,6))
+print("Operador Multiplicacion, 5 * 6 = ",5*6)
+print("Operador Division, 5 / 6 = ",5/6)
+print("Operador Division entera, 5 // 6 = ",5//6)
+print("Operador Módulo, 5 % 6 = ",5%6)
+print("Operador Exponente, 5 ** 6 = ",5**6)
+print("Operador +, +5 = ",+5)
+print("Operador -, -5 = ",-5)
